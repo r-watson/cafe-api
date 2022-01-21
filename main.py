08facbddd899e97df6dfa29b8d28a5700b16e349
r-watson <rtwatson86@gmail.com>
2022-01-21 15:06:42 -0500

    completed cafe api with link to postman documentation

--- main.py
+++ main.py
@@ -39,6 +39,14 @@ class Cafe(db.Model):
         ## Method 2 using dictionary comprehension to do the same thing.
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
+def make_bool(val: int) -> bool:
+    """
+    Take in a numberic value and converts to boolean
+
+    :param val: Expecting number
+    :return: Boolean
+    """
+    return bool(int(val))
 
 @app.route("/")
 def home():
@@ -101,10 +109,10 @@ def add_cafe():
             img_url=data["img_url"],
             location=data["location"],
             seats=data["seats"],
-            has_toilet=bool(data["has_toilet"]),
-            has_wifi=bool(data["has_wifi"]),
-            has_sockets=bool(data["has_sockets"]),
-            can_take_calls=bool(data["can_take_calls"]),
+            has_toilet=make_bool(data["has_toilet"]),
+            has_wifi=make_bool(data["has_wifi"]),
+            has_sockets=make_bool(data["has_sockets"]),
+            can_take_calls=make_bool(data["can_take_calls"]),
             coffee_price=data["coffee_price"],
         )
         db.session.add(new_cafe)
@@ -113,9 +121,38 @@ def add_cafe():
     return jsonify(error={"failed": "Sorry, you failed to add that cafe."})
 
 ## HTTP PUT/PATCH - Update Record
+## PUT Replaces the entire record
+## Patch replaces only one part of the record
+
+@app.route("/update-price/<cafe_id>", methods=["PATCH"])
+def update_price(cafe_id):
+    cafe_record = Cafe.query.get(cafe_id)
+    new_price = request.args.get('new_price')
+    if cafe_record:
+        cafe_record.coffee_price = new_price
+        db.session.commit()
+        ## Just add the code after the jsonify method to customize. 200 = OK
+        return jsonify(response={"success": "Successfully updated the price."}), 200
+    # 404 = Resource not found
+    return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
 
 ## HTTP DELETE - Delete Record
 
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def report_closed(cafe_id):
+    cafe_record = Cafe.query.get(cafe_id)
+    api_key = "TopSecretAPIKey"
+    api_key_entered = request.args.get("api-key")
+    if api_key_entered == api_key:
+        if cafe_record:
+            # If api_key is correct AND cafe_record exists then delete the record
+            db.session.delete(cafe_record)
+            db.session.commit()
+            return jsonify(reponse={"success": "Successfully deleted cafe."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry, a cafe with that ID was not found int he database."}), 404
+    else:
+        return jsonify(error={"error": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
 
 if __name__ == '__main__':
     app.run(debug=True)
